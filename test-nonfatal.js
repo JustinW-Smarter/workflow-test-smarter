5e070bb54234f923957a18b291637e4ac73a7047
justinwesterduin <westerduin.jwjob@gmail.com>
2025-05-08 14:07:23 +0200

    test expanding eslint action

--- test-nonfatal.js
+++ test-nonfatal.js
@@ -1,1 +1,20 @@
+// Ongebruikte variabele
 var a = 1
+var b = 2
+
+// Onjuiste inspringing
+function test() {
+    console.log('Hello World');
+}
+
+// Vergeten puntkomma
+var sum = a + b
+
+// Foutieve quotes
+var name = "John";
+
+// Verkeerde spaties rond operatoren
+var result= a+b;
+
+// Console.log waarschuwing (indien ingeschakeld in ESLint)
+console.log('This will be fixed by ESLint');

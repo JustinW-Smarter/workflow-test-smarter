b67154c79ce21587919014c85de0014e2a07e720
justinwesterduin <westerduin.jwjob@gmail.com>
2025-05-08 14:34:21 +0200

    add eslint fix rules

--- test-nonfatal.js
+++ test-nonfatal.js
@@ -1,20 +1,28 @@
-// Ongebruikte variabele
+// Ongebruikte variabele (niet fixbaar, alleen warning)
 var a = 1
 var b = 2
 
-// Onjuiste inspringing
-function test() {
-    console.log('Hello World');
+// Onjuiste inspringing en console.log
+function test(){
+console.log("Hello World")
 }
 
 // Vergeten puntkomma
 var sum = a + b
 
 // Foutieve quotes
-var name = "John";
+var name = "John"
 
 // Verkeerde spaties rond operatoren
-var result= a+b;
+var result= a+b
 
-// Console.log waarschuwing (indien ingeschakeld in ESLint)
-console.log('This will be fixed by ESLint');
+// Extra puntkomma
+var x = 5;;
+
+// Geen spatie tussen functie en haakjes
+const sayHi =()=>{console.log('Hi')}
+
+// Foutieve inspringing
+if(true){
+console.log('Yes');
+}
